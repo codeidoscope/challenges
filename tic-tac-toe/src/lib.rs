aa2f502350b3667b518cfb699cbbf8baf7099a00
Marion LV <marion@8thlight.com>
2019-03-30 21:12:35 +0000

    Add functionality to greet and prompt the user

--- tic-tac-toe/src/lib.rs
+++ tic-tac-toe/src/lib.rs
@@ -1,7 +1,24 @@
+fn greet_user() -> String {
+    String::from("Welcome to Tic Tac Toe! You are player X")
+}
+
+fn prompt_user() -> String {
+    String::from("Please select a position between 1 and 9: ")
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::greet_user;
+    use crate::prompt_user;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_greets_a_user_when_game_starts() {
+        assert_eq!(greet_user(), "Welcome to Tic Tac Toe! You are player X")
+    }
+
+    #[test]
+    fn it_prompts_a_user_to_choose_a_position() {
+        assert_eq!(prompt_user(), "Please select a position between 1 and 9: ")
     }
 }
