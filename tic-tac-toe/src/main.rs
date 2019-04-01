889f36c1b6ff5e300ff91864b5eecc769d59315c
Marion LV <marion@8thlight.com>
2019-04-01 10:16:49 +0100

    Use bin instead of lib and refactor board to be vector of Tiles

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -12,11 +12,34 @@ fn create_board(size: u32) -> Vec<String>{
     board
 }
 
+#[derive(Debug)]
+struct Tile {
+    symbol: String,
+    position: u32,
+}
+
+fn create_board_tiles(size: u32) -> Vec<Tile>{
+    let board_size: u32 = size * size;
+    let mut board: Vec<Tile> = Vec::new();
+
+    for i in 0..board_size {
+        board.push(Tile{symbol: format!("[{}] ", i+1), position: i})
+    }
+    println!("{:?}", board);
+    board
+}
+
+fn main() {
+    println!("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::display_prompt;
     use std::collections::HashMap;
+
+    use crate::display_prompt;
     use crate::create_board;
+    use crate::create_board_tiles;
 
     #[test]
     fn it_displays_a_prompt_to_the_user() {
@@ -27,6 +50,7 @@ mod tests {
         let welcome_prompt = prompts.get(&"welcome").unwrap();
         let select_position_prompt = prompts.get(&"select_position").unwrap();
 
+        print!("{}", display_prompt(welcome_prompt));
         assert_eq!(display_prompt(welcome_prompt),
                    "Welcome to Tic Tac Toe! You are player X");
         assert_eq!(display_prompt(select_position_prompt),
@@ -36,16 +60,27 @@ mod tests {
     #[test]
     fn it_creates_a_3_by_3_board() {
         assert_eq!(create_board(3), ["[1] ", "[2] ", "[3] ",
-                                     "[4] ", "[5] ", "[6] ",
-                                     "[7] ", "[8] ", "[9] "])
+            "[4] ", "[5] ", "[6] ",
+            "[7] ", "[8] ", "[9] "])
     }
 
     #[test]
     fn it_creates_a_4_by_4_board() {
         assert_eq!(create_board(4), ["[1] ", "[2] ", "[3] ", "[4] ",
-                                     "[5] ", "[6] ", "[7] ", "[8] ",
-                                     "[9] ", "[10] ", "[11] ", "[12] ",
-                                     "[13] ", "[14] ", "[15] ", "[16] "])
+            "[5] ", "[6] ", "[7] ", "[8] ",
+            "[9] ", "[10] ", "[11] ", "[12] ",
+            "[13] ", "[14] ", "[15] ", "[16] "])
     }
 
+    #[test]
+    fn it_creates_a_3_by_3_board_of_tiles() {
+        let board = create_board_tiles(3);
+        assert_eq!(board.len(), 9)
+    }
+
+    #[test]
+    fn it_creates_a_4_by_4_board_of_tiles() {
+        let board = create_board_tiles(4);
+        assert_eq!(board.len(), 16)
+    }
 }
