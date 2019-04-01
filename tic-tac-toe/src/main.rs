c60818be3ae1cb42788aa96d2174edf0b33f1b86
Marion LV <marion@8thlight.com>
2019-04-01 10:18:00 +0100

    Remove previous create_board function

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -2,23 +2,13 @@ fn display_prompt(prompt: &str) -> &str {
     prompt
 }
 
-fn create_board(size: u32) -> Vec<String>{
-    let board_size: u32 = size * size;
-    let mut board: Vec<String> = Vec::new();
-
-    for i in 0..board_size {
-        board.push(format!("[{}] ", i+1))
-    }
-    board
-}
-
 #[derive(Debug)]
 struct Tile {
     symbol: String,
     position: u32,
 }
 
-fn create_board_tiles(size: u32) -> Vec<Tile>{
+fn create_board(size: u32) -> Vec<Tile>{
     let board_size: u32 = size * size;
     let mut board: Vec<Tile> = Vec::new();
 
@@ -39,7 +29,6 @@ mod tests {
 
     use crate::display_prompt;
     use crate::create_board;
-    use crate::create_board_tiles;
 
     #[test]
     fn it_displays_a_prompt_to_the_user() {
@@ -58,29 +47,14 @@ mod tests {
     }
 
     #[test]
-    fn it_creates_a_3_by_3_board() {
-        assert_eq!(create_board(3), ["[1] ", "[2] ", "[3] ",
-            "[4] ", "[5] ", "[6] ",
-            "[7] ", "[8] ", "[9] "])
-    }
-
-    #[test]
-    fn it_creates_a_4_by_4_board() {
-        assert_eq!(create_board(4), ["[1] ", "[2] ", "[3] ", "[4] ",
-            "[5] ", "[6] ", "[7] ", "[8] ",
-            "[9] ", "[10] ", "[11] ", "[12] ",
-            "[13] ", "[14] ", "[15] ", "[16] "])
-    }
-
-    #[test]
     fn it_creates_a_3_by_3_board_of_tiles() {
-        let board = create_board_tiles(3);
+        let board = create_board(3);
         assert_eq!(board.len(), 9)
     }
 
     #[test]
     fn it_creates_a_4_by_4_board_of_tiles() {
-        let board = create_board_tiles(4);
+        let board = create_board(4);
         assert_eq!(board.len(), 16)
     }
 }
